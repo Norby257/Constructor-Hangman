a583929ede2dda37dfff7b26df837224d8f78890
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-09 18:59:20 -0600

    re-did letter constructor

--- letter.js
+++ letter.js
@@ -1,22 +1,23 @@
-//constructor 
-//just a placeholder param in the below for now 
-function letter(letter) {
-    this.letter = " ";  //how to store string val 
-    this.guessed = false //boolean to see if letter is guest 
-    this.guessedLetter = function() {
-       if (letter) {
-           return letter;
-       }
-       else console.log("_");
-    }
-
-    this.checkLetter = function(letter) {
-       //pseudocody 
-        if (letter === letter2) {
-           this.guessed = true;
+var Letter = function(character) {
+    //string val to store underlying char / on key up?
+    //boolean
+    //display char to user
+    this.displayChar = function() {
+        if (character) {
+            console.log(character);
+        } else {
+            console.log("_");
         }
-        
+    };
+
+    this.checkChar  = function() {
+        if (character = computerCharacter) {
+            character.guessed = true;
+        }
     }
 
 
 }
+
+
+module.exports = Letter;
