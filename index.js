e142b1542f488eeaea1e02d78e769e5439a3c87d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-09 19:34:33 -0600

    added logic to file

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const promptUser = require('inquirer');
+// const promptUser = require('inquirer');
+
+
+//something with argv and how to store what letter they pressed?
+const letter = require("./letter");
+const word = require("./word");
+
+ console.log("Guess a letter!");
+//array of words that I have  scott pilgrim themed 
+
+randomWord();
+
+
+//function that picks a random word 
+    function randomWord(wordBank) {
+        var wordBank = ["Clash at Demonhead", "Bomb-ob", "Ramona", "Scott", "Guitar", "Vegan Police", "Envy Adams", "Todd", "Young Neil"];        
+        let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+        // console.log(randomWord);
+        //split that word into an array
+        console.log(randomWord.split(""));
+
+    }
+
+
+
+
